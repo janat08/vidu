f3a6f519b1048846126e9454ad0ae0c4be2009e5
janat08 <jey.and.key@gmail.com>
2019-02-24 16:13:27 +0600

    setup

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-import {makeDOMDriver} from '@cycle/dom';
-import { run } from '@cycle/run';
-import { withState } from '@cycle/state';
 
 var app = require('./src/app.js');
-
 
 if (module.hot) {
   module.hot.accept('./src/app.js', () => {
